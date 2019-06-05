a8c7e2f8fd69bbe3973725cc7a1a15a12c18674e
Alexandre MEUR <alex.meur@gmail.com>
2019-06-05 12:33:15 +0200

    Utilisation de Spring à la place de ObjectFactory pour gérer les créations d'objets

--- src/main/java/com/training/springcore/BigCorpApplication.java
+++ src/main/java/com/training/springcore/BigCorpApplication.java
@@ -1,8 +1,9 @@
 package com.training.springcore;
 
-import com.training.spring.bigCorp.ObjectFactory;
 import com.training.springcore.service.SiteService;
 import com.training.springcore.service.SiteServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BigCorpApplication {
 
@@ -12,9 +13,14 @@ public class BigCorpApplication {
     }
 
     public void run(){
+        //L'initialisation de siteService n'est faite qu'une fois
+        //Avec scope="prototype" dans beans.xml, on peut l'instancier plusieurs fois
+        //Avec lazy-init="true", l'instance n'est créée que quand on en a besoin (pendant le runtime)
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         System.out.println("Application startup");
-        ObjectFactory factory = new ObjectFactory();
-        SiteService siteService = factory.createSiteService();
+        SiteService siteService = context.getBean(SiteService.class);
         System.out.println(siteService.findById("siteA"));
+        SiteService siteService2 = context.getBean(SiteService.class);
+        System.out.println(siteService2.findById("siteA"));
     }
 }
